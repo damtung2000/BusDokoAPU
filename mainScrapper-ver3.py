f0c55f738405504bfe780e990c5cc1f418b9a8ea
Tung Dam <damtung2000@gmail.com>
2021-09-29 23:05:33 +0900

    Rename variables

--- mainScrapper-ver3.py
+++ mainScrapper-ver3.py
@@ -12,21 +12,22 @@ from signal import pause
 # Modify this if you have a different sized character LCD
 lcd_columns = 16
 lcd_rows = 2
-  
+
 # Raspberry Pi Pin Config:
 lcd_rs = digitalio.DigitalInOut(board.D26)
-lcd_en = digitalio.DigitalInOut(board.D19) 
+lcd_en = digitalio.DigitalInOut(board.D19)
 lcd_d7 = digitalio.DigitalInOut(board.D27)
 lcd_d6 = digitalio.DigitalInOut(board.D22)
 lcd_d5 = digitalio.DigitalInOut(board.D24)
 lcd_d4 = digitalio.DigitalInOut(board.D25)
 lcd_backlight = digitalio.DigitalInOut(board.D4)
- 
+
 # Initialise the lcd class
 lcd = characterlcd.Character_LCD_Mono(
-     lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight
+    lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight
 )
-bun = (0b00000,
+japaneseMinuteCharacter = (
+    0b00000,
     0b01010,
     0b10001,
     0b11110,
@@ -35,77 +36,123 @@ bun = (0b00000,
     0b10100,
     0b00000,
 )
-lcd.create_char(0,bun)
+lcd.create_char(0, japaneseMinuteCharacter)
 # Minami-APU
 lcd.message = "Loading\nMinami-APU"
-getSchedule("https://www.busdoko-oita.jp/map/SpecialRoute/Route?spId=1&drId=1&stSid=a3526885-da77-43dc-9bc3-3cfe3a7b1999",'Minami-APU')
+getSchedule(
+    "https://www.busdoko-oita.jp/map/SpecialRoute/Route?spId=1&drId=1&stSid=a3526885-da77-43dc-9bc3-3cfe3a7b1999",
+    "Minami-APU",
+)
 
 # Minami-Eki
 lcd.message = "Loading\nMinami-Eki"
-getSchedule("https://www.busdoko-oita.jp/map/SpecialRoute/Route?spId=1&drId=2&stSid=e64c5d40-62af-4efd-b242-da26ae5502dd", 'Minami-Eki')
+getSchedule(
+    "https://www.busdoko-oita.jp/map/SpecialRoute/Route?spId=1&drId=2&stSid=e64c5d40-62af-4efd-b242-da26ae5502dd",
+    "Minami-Eki",
+)
 
 # Daisan-APU
 lcd.message = "Loading\nDaisan-APU"
-getSchedule("https://www.busdoko-oita.jp/map/SpecialRoute/Route?spId=1&drId=1&stSid=924e8147-7112-437e-9fe8-a6a3bb8e07fd", 'Daisan-APU')
+getSchedule(
+    "https://www.busdoko-oita.jp/map/SpecialRoute/Route?spId=1&drId=1&stSid=924e8147-7112-437e-9fe8-a6a3bb8e07fd",
+    "Daisan-APU",
+)
 
 # Daisan-Eki
 lcd.message = "Loading\nDaisan-APU"
-getSchedule("https://www.busdoko-oita.jp/map/SpecialRoute/Route?spId=1&drId=1&stSid=924e8147-7112-437e-9fe8-a6a3bb8e07fd", 'Daisan-Eki')
+getSchedule(
+    "https://www.busdoko-oita.jp/map/SpecialRoute/Route?spId=1&drId=1&stSid=924e8147-7112-437e-9fe8-a6a3bb8e07fd",
+    "Daisan-Eki",
+)
+
 
 def readData(station):
-    data = pd.read_json(station + '.json',orient='columns')
+    data = pd.read_json(station + ".json", orient="columns")
     return data
 
-def getIndex(arrival):
+
+def getIndex(arrivalTime):
     # print(arrival)
-    indx = 0
-    for i in arrival:
+    index = 0
+    for time in arrivalTime:
         now = datetime.now()
-        then = datetime.combine(now, datetime.strptime(i, '%H:%M').time())
+        then = datetime.combine(now, datetime.strptime(time, "%H:%M").time())
         minutesLeft = (then - now) // timedelta(minutes=1)
-        if (minutesLeft > 0):
-            indx = arrival.index(i)
+        if minutesLeft > 0:
+            index = arrivalTime.index(time)
             break
-    if indx == (len(arrival)-1):
-        print(i)
-        return indx
-    elif indx < (len(arrival)-1):
-        nextBusindx = indx + 1
-        print(i, arrival[nextBusindx])
-        return indx, nextBusindx
+    if index == (len(arrivalTime) - 1):
+        print(time)
+        return index
+    elif index < (len(arrivalTime) - 1):
+        nextBusIndex = index + 1
+        print(time, arrivalTime[nextBusIndex])
+        return index, nextBusIndex
+
+
 def minamiEkibutton():
-    data = readData('Minami-Eki')
-    arrival = pd.Series.tolist(data['ArrivalTime'])
-    busno = pd.Series.tolist(data['BusNo'])
-    state = pd.Series.tolist(data['BusState'])
-    indx = getIndex(arrival)
-    State = []
+    data = readData("Minami-Eki")
+    arrivalTime = pd.Series.tolist(data["ArrivalTime"])
+    busNumber = pd.Series.tolist(data["BusNo"])
+    busState = pd.Series.tolist(data["BusState"])
+    index = getIndex(arrivalTime)
+    currentBusState = []
     print(data)
-    for i in state:
+    for i in busState:
         if str(i) == "":
-            State.append(i)
+            currentBusState.append(i)
         elif str(i).strip() == "通常運行":
-            State.append("On time")
+            currentBusState.append("On time")
         else:
-            res = regex.findall("(\d+)",str(i))
-            State.append(str(res[0]) + "\x00" + " late")
-            
-    if type(indx) is tuple:
-        print(busno[indx[0]],' ', arrival[indx[0]], ' ', State[indx[0]])
-        print(busno[indx[1]],' ', arrival[indx[1]], ' ', State[indx[1]])
+            res = regex.findall("(\d+)", str(i))
+            currentBusState.append(str(res[0]) + "\x00" + " late")
+
+    if type(index) is tuple:
+        print(
+            busNumber[index[0]],
+            " ",
+            arrivalTime[index[0]],
+            " ",
+            currentBusState[index[0]],
+        )
+        print(
+            busNumber[index[1]],
+            " ",
+            arrivalTime[index[1]],
+            " ",
+            currentBusState[index[1]],
+        )
         lcd.clear()
-        lcd.message = str(busno[indx[0]])+ ' ' + str(arrival[indx[0]]) + ' ' + str(State[indx[0]]) + '\n' + str(busno[indx[1]]) + ' ' +str(arrival[indx[1]]) + ' ' + str(State[indx[1]])
+        lcd.message = (
+            str(busNumber[index[0]])
+            + " "
+            + str(arrivalTime[index[0]])
+            + " "
+            + str(currentBusState[index[0]])
+            + "\n"
+            + str(busNumber[index[1]])
+            + " "
+            + str(arrivalTime[index[1]])
+            + " "
+            + str(currentBusState[index[1]])
+        )
     else:
-        print(busno[indx],' ', arrival[indx], ' ', State[indx])
+        print(busNumber[index], " ", arrivalTime[index], " ", currentBusState[index])
         lcd.clear()
-        lcd.message= str(busno[indx]) + ' ' + str(arrival[indx]) + ' ' + str(State[indx])
+        lcd.message = (
+            str(busNumber[index])
+            + " "
+            + str(arrivalTime[index])
+            + " "
+            + str(currentBusState[index])
+        )
+
+
 def clearButton():
     lcd.clear()
 
-minamiButton = io.Button(21,pull_up=False, hold_time=0.5)
+
+minamiButton = io.Button(21, pull_up=False, hold_time=0.5)
 minamiButton.when_pressed = minamiEkibutton
 minamiButton.when_held = clearButton
 pause()
-    
-
-
--- scrapper.py
+++ scrapper.py
@@ -1,57 +1,64 @@
 import time
+
 start = time.time()
-#from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import pandas as pd
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
 chrome_options = Options()
 chrome_options.add_argument("--headless")
-def getSchedule(url,name):
+
+
+def getSchedule(url, name):
     driver = webdriver.Chrome(options=chrome_options)
     driver.get(url)
-#     english = driver.find_element_by_class_name('languageButton')
-#     english.click()
-#     English = driver.find_element_by_id('modal-language-en')
-#     English.click()
+    #     english = driver.find_element_by_class_name('languageButton')
+    #     english.click()
+    #     English = driver.find_element_by_id('modal-language-en')
+    #     English.click()
     try:
         element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CLASS_NAME, "arrivalTime")))
-        arrivalTime = driver.find_elements_by_class_name('arrivalTime')
+            EC.presence_of_element_located((By.CLASS_NAME, "arrivalTime"))
+        )
+        arrivalTimeElements = driver.find_elements_by_class_name("arrivalTime")
 
-        busNo = driver.find_elements_by_class_name('courseNo')
-    # busNum = driver.find_elements_by_xpath('//*[@class="courseNo"]/div')
-    
-        busState = driver.find_elements_by_class_name('busState')
-#         for i in arrivalTime:
-#             print(i.text)
-#         for i in busNo:
-#             print(i.text)
-#         for i in busState:
-#             print(i.text)
-        arrivaltime = []
-        busno = []
-        busstate = []
+        busNumberElements = driver.find_elements_by_class_name("courseNo")
+
+        busStateElements = driver.find_elements_by_class_name("busState")
+        #         for i in arrivalTime:
+        #             print(i.text)
+        #         for i in busNo:
+        #             print(i.text)
+        #         for i in busState:
+        #             print(i.text)
+        arrivalTime = []
+        busNumber = []
+        busState = []
         busCondition = []
         i = 0
-        for i in arrivalTime:
-            arrivaltime.append(i.text)
-        for i in busNo:
-            busno.append(i.text[0:2])
-        for i in busState:
-            busstate.append(i.text)
-        busCondition = {'BusNo':busno,'ArrivalTime':arrivaltime,'BusState':busstate}
+        for i in arrivalTimeElements:
+            arrivalTime.append(i.text)
+        for i in busNumberElements:
+            busNumber.append(i.text[0:2])
+        for i in busStateElements:
+            busState.append(i.text)
+        busCondition = {
+            "BusNo": busNumber,
+            "ArrivalTime": arrivalTime,
+            "BusState": busState,
+        }
 
-    #     busCondition = list(zip(busno,arrivaltime,busstate))
-    # columns = ["BusNo","ArrivalTime","BusState"]
+        #     busCondition = list(zip(busno,arrivaltime,busstate))
+        # columns = ["BusNo","ArrivalTime","BusState"]
 
-        #apply result to dataframe
-        df=pd.DataFrame(busCondition)
+        # apply result to dataframe
+        df = pd.DataFrame(busCondition)
         # write to json
-        df.to_json('%s.json' % name)
+        df.to_json("%s.json" % name)
         print(df)
-        print(time.time()-start)
+        print(time.time() - start)
     finally:
         driver.quit()
